a44e48f8829d0a7bba86a24b7e3aca6c1ab748d0
Matthew Bronstein <mbronstein1@gmail.com>
2022-10-24 15:48:28 -0500

    updated connection.js with future mongodb_uri connection

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
+
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmediaDB';
 
 //Wrap Mongoose around local connection to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/socialmediaDB', {
+connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-module.exports = mongoose.connection;
+module.exports = connection;
